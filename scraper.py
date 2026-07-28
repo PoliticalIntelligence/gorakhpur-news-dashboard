b94123f926f4efe8c70eabf2eb8028770550de0e
Eliza Roy <priyansh.shrivastava@inverv.com>
2026-07-28 17:54:27 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -387,7 +387,7 @@ def main():
             "Topic",
             "Summary",
             "Headline",
-            "Thumbnail"
+            "Thumbnail",
             "URL",
             "Content"
         ]
